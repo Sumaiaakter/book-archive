666d6f848937904b4c1dcf9c5bc6b7079609c2ef
sumaiaakter <sumaiamannan2013@gmail.com>
2021-09-02 09:56:21 -0500

    submit new code

--- api.js
+++ api.js
@@ -1,98 +1,65 @@
+
+
 const searchBook = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    // clear data
+    console.log(searchText);
 
     searchField.value = "";
 
-    // load data
-    const url = ` http://openlibrary.org/search.json?q=${searchText}`;
 
-    console.log(url);
-    fetch(url)
+    const bookUrl = `http://openlibrary.org/search.json?q=${searchText}`;
+
+    // console.log(bookUrl);
+    fetch(bookUrl)
         .then(res => res.json())
-        .then(data => displayBook(data.docs[0]));
+        .then(data => displayBook(data.docs))
 }
-// searchBook();
 
-const displayBook = books => {
+const displayData = data => {
+    console.log(data);
+    if (searchField.value === '') {
+        const error = document.createElement('p');
+        error.innerHTML = `
+        <h3 class="text-white text-center bg-danger mx-auto w-50">Please enter valid name</h3>
 
-    console.log(books);
-
-
-    // images collection
-
-    const load = () => {
-        const url = "https://ia902701.us.archive.org/view_archive.php?archive=/18/items/olcovers55/olcovers55-L.zip&file=554106-L.jpg";
-        console.log(url);
-        const movieContainer = document.getElementById("image-details")
-        const movieBox = document.createElement("div")
-        movieBox.classList.add("col-md-3")
-        movieBox.innerHTML = `
-            <div class="shadow rounded p-3 m-2">
-                <img width="200px" height="200px" class="img-fluid" src='${url}'>
-
-            </div>
-`
-
-        movieContainer.appendChild(movieBox)
-
-
-    }
-    load();
-
-
-
-
-    const bookName = document.getElementById('author-name');
-    const movieContainer = document.getElementById("image-details");
-    const url = "https://ia902701.us.archive.org/${554106}-L.jpg";
-
-    // clear div
-
-    bookName.innerHTML = "";
-
-    // error message
-
-    if (!books) {
-        document.getElementById("error-message").innerHTML =
-            "<p class='text-center p-3 bg-danger'><b>Please enter a valid name...</b></p>";
+        `;
+        errorHnadle.appendChild(error);
     }
     else {
 
-        const div = document.createElement('div');
-        div.classList.add('col-md-3');
-
-        div.innerHTML = `
-           
-            <div class="shadow rounded p-2 m-2">
-                 
-                <h5 class="card-title">Name: ${books.title}</h5>
-                <p class="card-text">First Publish: ${books.first_publish_year}</p>
-                <p class="card-text">Publisher: ${books.publisher}</p>
-                <p class="card-text">Author Name: ${books.author_name}</p>
-
-            </div>
-           
-
-    `;
-        // bookName.appendChild(div);
-        movieContainer.appendChild(div);
-
     }
-
-
-
-
-
 }
 
+const displayBook = docs => {
+    // console.log(docs)
+
+    const searchResult = document.getElementById('search-result');
+    // clear div
+    searchResult.innerHTML = "";
 
 
+    docs.forEach(doc => {
+        console.log(doc);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card">
+                <img src="https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Title: ${doc.title}</h5>
+                    <p class="card-text">Author Name: ${doc.author_name}</p>
+                    <p class="card-text">First Publish: ${doc.first_publish_year}</p>
+                    <p class="card-text">Publisher: ${doc.publisher}</p>
+                </div>
+            </div>
+
+        `;
+        searchResult.appendChild(div);
 
 
+    })
 
-
-
+}
 
 
